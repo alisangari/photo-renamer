272c63ac34e2318664d1a13e405c9ee2bb91d1a4
alisangari <a_b_sangari@yahoo.com>
2017-01-22 10:20:30 -0500

    almost done

--- src/gui/MainWindow.java
+++ src/gui/MainWindow.java
@@ -1,19 +1,23 @@
 package gui;
 
-import java.awt.Dialog;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collections;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import com.jgoodies.forms.layout.ColumnSpec;
 import com.jgoodies.forms.layout.FormLayout;
@@ -21,9 +25,11 @@ import com.jgoodies.forms.layout.FormSpecs;
 import com.jgoodies.forms.layout.RowSpec;
 //import /ui.ProgressBar;
 
+import services.RenameService;
+
 public class MainWindow {
 
-	private JFrame frame;
+	JFrame frame;
 	private JTextField txtPrefix;
 	private JTextField txtPostfix;
 	private JLabel lblFileCount;
@@ -34,28 +40,35 @@ public class MainWindow {
 	private JCheckBox cbTime;
 	private JCheckBox cbLocation;
 
-	
-	
 	private JFileChooser chooser;
 	private String newFolder = "";
 	private boolean canceled = false;
 	private ArrayList<File> files = new ArrayList<File>();
 	private String choosertitle;
 	private String imagesFolder;
-	
-	
+	private boolean progressFlag;
+
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		;
+		setUIFont(new javax.swing.plaf.FontUIResource(new JLabel("").getFont().getFontName(), Font.PLAIN, 16));
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					MainWindow window = new MainWindow();
 					window.frame.setVisible(true);
-					
-//					ProgressBar pBar = new ProgressBar();
-//					pBar.setVisible(true);
+
+					// ProgressBar pBar = new ProgressBar();
+					// pBar.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -63,55 +76,50 @@ public class MainWindow {
 		});
 	}
 
+	public static void setUIFont(javax.swing.plaf.FontUIResource f) {
+		java.util.Enumeration keys = UIManager.getDefaults().keys();
+		while (keys.hasMoreElements()) {
+			Object key = keys.nextElement();
+			Object value = UIManager.get(key);
+			if (value != null && value instanceof javax.swing.plaf.FontUIResource)
+				UIManager.put(key, f);
+		}
+	}
+
 	/**
 	 * Create the application.
 	 */
 	public MainWindow() {
 		initialize();
+		runThread();
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
+		progressFlag = false;
 		frame = new JFrame();
-		frame.setTitle("TravPic - See where you took that picture :)");
-		frame.setBounds(100, 100, 669, 472);
+		frame.setTitle("TravPic - See where you took that picture");
+		frame.setBounds(100, 100, 873, 430);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.getContentPane().setLayout(new FormLayout(new ColumnSpec[] {
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				FormSpecs.DEFAULT_COLSPEC,
-				FormSpecs.RELATED_GAP_COLSPEC,
-				ColumnSpec.decode("default:grow"),},
-			new RowSpec[] {
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,
-				FormSpecs.RELATED_GAP_ROWSPEC,
-				FormSpecs.DEFAULT_ROWSPEC,}));
-		
+		frame.getContentPane()
+				.setLayout(new FormLayout(
+						new ColumnSpec[] { FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC,
+								FormSpecs.RELATED_GAP_COLSPEC, FormSpecs.DEFAULT_COLSPEC, FormSpecs.RELATED_GAP_COLSPEC,
+								ColumnSpec.decode("default:grow"), },
+						new RowSpec[] { FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+								FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+								FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+								FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+								FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+								FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC,
+								FormSpecs.DEFAULT_ROWSPEC, FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC,
+								FormSpecs.RELATED_GAP_ROWSPEC, FormSpecs.DEFAULT_ROWSPEC, }));
+
 		JLabel lblSelectTheFolder = new JLabel("Select the folder that contains your pictures");
 		frame.getContentPane().add(lblSelectTheFolder, "2, 2");
-		
+
 		btnBrowse = new JButton("Browse...");
 		btnBrowse.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -119,45 +127,45 @@ public class MainWindow {
 			}
 		});
 		frame.getContentPane().add(btnBrowse, "6, 2");
-		
+
 		lblFileCount = new JLabel("");
 		frame.getContentPane().add(lblFileCount, "6, 4");
-		
+
 		JLabel lblSetAPrefix = new JLabel("Set a prefix for your pictures");
 		frame.getContentPane().add(lblSetAPrefix, "2, 8");
-		
+
 		txtPrefix = new JTextField();
 		frame.getContentPane().add(txtPrefix, "6, 8, fill, default");
 		txtPrefix.setColumns(10);
-		
+
 		JLabel lblSetAPostfix = new JLabel("Set a post-fix for your pictures");
 		frame.getContentPane().add(lblSetAPostfix, "2, 10");
-		
+
 		txtPostfix = new JTextField();
 		frame.getContentPane().add(txtPostfix, "6, 10, fill, default");
 		txtPostfix.setColumns(10);
-		
+
 		JLabel lblIncludeDateIn = new JLabel("Include date in picture names");
 		frame.getContentPane().add(lblIncludeDateIn, "2, 12");
-		
+
 		cbDate = new JCheckBox("");
 		frame.getContentPane().add(cbDate, "6, 12");
-		
+
 		JLabel lblIncludeTimeIn = new JLabel("Include time in picture names");
 		frame.getContentPane().add(lblIncludeTimeIn, "2, 14");
-		
+
 		cbTime = new JCheckBox("");
 		frame.getContentPane().add(cbTime, "6, 14");
-		
+
 		JLabel lblIncludeLocationIn = new JLabel("Include location in picture names");
 		frame.getContentPane().add(lblIncludeLocationIn, "2, 16");
-		
+
 		cbLocation = new JCheckBox("");
 		frame.getContentPane().add(cbLocation, "6, 16");
-		
+
 		JLabel lblSampleName = new JLabel("Sample file name: prefix 15-1-2017 at 10-25 Location post-fix.jpg");
 		frame.getContentPane().add(lblSampleName, "6, 18");
-		
+
 		btnClearForm = new JButton("Clear form");
 		btnClearForm.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -165,115 +173,115 @@ public class MainWindow {
 			}
 		});
 		frame.getContentPane().add(btnClearForm, "2, 22");
-		
+
 		btnStart = new JButton("Start");
 		btnStart.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				startP();
+				setProgressFlag();
 			}
 		});
 		frame.getContentPane().add(btnStart, "6, 22");
 	}
 
 
-
-
-
-
-
-
-
-
-
-
-
-	//************ METHODS **************
-		protected void browse() {
-			chooser = new JFileChooser();
-			chooser.setCurrentDirectory(new java.io.File("."));
-			chooser.setDialogTitle(choosertitle);
-			chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-			//
-			// disable the "All files" option.
-			//
-			chooser.setAcceptAllFileFilterUsed(false);
-			//
-			if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
-				// System.out.println("getCurrentDirectory(): " +
-				// chooser.getCurrentDirectory());
-				// System.out.println("getSelectedFile() : " +
-				// chooser.getSelectedFile());
-				imagesFolder = chooser.getSelectedFile().getAbsolutePath();
-				files = new ArrayList<File>();
-				listf(imagesFolder, files);
-//				btnStart.setEnabled(true);
-//				btnBrowse.setEnabled(false);
-			} else {
-				// System.out.println("No Selection ");
-				imagesFolder = null;
-			}
-			Collections.sort(files);
-			lblFileCount.setText("You have selected "+imagesFolder+" which contains "+files.size()+" files.");
-//			System.out.println(files.size() + "files have been selected.");
-		}
-
-		private void startP(){
-			Progress progress = new Progress(files, getPrefix(), getPostfix(),cbDate.isSelected(), cbTime.isSelected(), cbLocation.isSelected());
-			progress.setLocationRelativeTo(frame);
-			progress.startP();
-			progress.setModal(true);
-			progress.setVisible(true);
-//			progress.startP();
-		}
-		/*protected void start() {
-			ProgressBar progressBar = new ProgressBar();
-			progressBar.setTotalFileCount(files.size());
-			progressBar.setVisible(true);
-//			btnClearForm.setEnabled(true);
-//			btnBrowse.setEnabled(false);
-			int c = 1;
-			for (File file : files) {
-//				System.out.println("file " + c++ + "("+ file.getName() +")"+ " sent for processing...");
-				if (!progressBar.isCancelled()) {
-					progressBar.update(c);
-					Rename.start(
-							file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - file.getName().length()),
-							file.getName(),getPrefix(), getPostfix(),cbDate.isSelected(), cbTime.isSelected(), cbLocation.isSelected());
+	private void runThread() {
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				while (true) {
+					System.out.println();
+					if (progressFlag) {
+						startProcess();
+					}
 				}
 			}
-//			System.out.println("Rename process of " + files.size() + " files Completed!");
+		});
+		t.start();
+	}
 
-		}*/
+	private void setProgressFlag() {
+		progressFlag = true;
 
-		private String getPostfix() {
-			return txtPostfix.getText();
+	}
+
+	protected void browse() {
+		chooser = new JFileChooser();
+		chooser.setCurrentDirectory(new java.io.File("."));
+		chooser.setDialogTitle(choosertitle);
+		chooser.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+		chooser.setAcceptAllFileFilterUsed(false);
+
+		if (chooser.showOpenDialog(chooser) == JFileChooser.APPROVE_OPTION) {
+			imagesFolder = chooser.getSelectedFile().getAbsolutePath();
+			files = new ArrayList<File>();
+			listf(imagesFolder, files);
+		} else {
+			imagesFolder = null;
 		}
+		Collections.sort(files);
+		lblFileCount.setText("You have selected " + imagesFolder + " which contains " + files.size() + " files.");
+	}
 
-		private String getPrefix() {
-			return txtPrefix.getText();
-		}
+	private void startProcess() {
+		if (files.size() > 0) {
+			progressFlag = false;
+			Progress2 dlg = new Progress2(frame, files.size() - 1);
+			Thread t = new Thread(new Runnable() {
+				public void run() {
+					dlg.setVisible(true);
+				}
+			});
+			t.start();
+			dlg.lblStatus.setText("In progress...");
+			for (int i = 0; i <= files.size() - 1; i++) {
+				dlg.lblProgress.setText("Processing file " + (i + 1) + " of " + files.size() + " ...");
+				processImage(files.get(i));
 
-		protected void clearForm() {
-			lblFileCount.setText("");
-			txtPostfix.setText("");
-			txtPrefix.setText("");
-			
-
-		}
-
-		private void listf(String directoryName, ArrayList<File> files) {
-			File directory = new File(directoryName);
-
-			// get all the files from a directory
-			File[] fList = directory.listFiles();
-			for (File file : fList) {
-				if (file.isFile()) {
-					files.add(file);
-				} else if (file.isDirectory()) {
-//					newFolder = file.getAbsolutePath();
-//					listf(file.getAbsolutePath(), files);
+				dlg.dpb.setValue(i);
+				if (dlg.dpb.getValue() == files.size() - 1) {
+					dlg.setVisible(false);
+					dlg.lblProgress.setText(" ");
+					dlg.lblStatus.setText("Completed!");
+				}
+				try {
+					Thread.sleep(25);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
 				}
 			}
+			dlg.setVisible(true);
 		}
+	}
+
+	public void processImage(File file) {
+		RenameService.start(
+				file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - file.getName().length()),
+				file.getName(), txtPrefix.getText(), txtPostfix.getText(), cbDate.isSelected(), cbTime.isSelected(),
+				cbLocation.isSelected());
+	}
+
+	protected void clearForm() {
+		lblFileCount.setText("");
+		txtPostfix.setText("");
+		txtPrefix.setText("");
+		cbDate.setSelected(false);
+		cbTime.setSelected(false);
+		cbLocation.setSelected(false);
+	}
+
+	private void listf(String directoryName, ArrayList<File> files) {
+		File directory = new File(directoryName);
+
+		// get all the files from a directory
+		File[] fList = directory.listFiles();
+		for (File file : fList) {
+			if (file.isFile()) {
+				files.add(file);
+			} else if (file.isDirectory()) {
+				// newFolder = file.getAbsolutePath();
+				// listf(file.getAbsolutePath(), files);
+			}
+		}
+	}
 
 }
--- src/gui/Progress.java
+++ src/gui/Progress.java
@@ -1,132 +1,30 @@
 package gui;
 
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-import java.io.File;
-import java.util.ArrayList;
 
-import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JPanel;
 import javax.swing.JProgressBar;
-import javax.swing.border.EmptyBorder;
-
-import services.RenameService;
 
 public class Progress extends JDialog {
 
-	private final JPanel contentPanel = new JPanel();
-	private JProgressBar progressBar;
-	private JLabel lblStatus;
-
-	private ArrayList<File> files;
-	private String prefix;
-	private String postfix;
-	private boolean includeDate;
-	private boolean includeTime;
-	private boolean includeLocation;
-
+	JProgressBar dpb;
+	JLabel lblProgress;
+	JLabel lblStatus;
 	
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		try {
-			Progress dialog = new Progress();
-			dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-			dialog.setVisible(true);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
-
-	public Progress() {
-	}
-
-	/**
-	 * Create the dialog.
-	 * 
-	 * @param files
-	 * @param d
-	 * @param c
-	 * @param b
-	 * @param string2
-	 * @param string
-	 */
-	public Progress(ArrayList<File> files, String prefix, String postfix, boolean includeDate, boolean includeTime,
-			boolean includeLocation) {
-		this.files = files;
-		this.prefix=prefix;
-		this.postfix=postfix;
-		this.includeDate=includeDate;
-		this.includeTime=includeTime;
-		this.includeLocation=includeLocation;
+	public Progress(JFrame parentFrame, int maxCount){
 		
-		setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
-		getContentPane().add(contentPanel, BorderLayout.CENTER);
-		GridBagLayout gbl_contentPanel = new GridBagLayout();
-		gbl_contentPanel.columnWidths = new int[] { 0, 0 };
-		gbl_contentPanel.rowHeights = new int[] { 0, 0, 0, 0 };
-		gbl_contentPanel.columnWeights = new double[] { 0.0, Double.MIN_VALUE };
-		gbl_contentPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
-		contentPanel.setLayout(gbl_contentPanel);
-		{
-			progressBar = new JProgressBar();
-			GridBagConstraints gbc_progressBar = new GridBagConstraints();
-			gbc_progressBar.fill = GridBagConstraints.HORIZONTAL;
-			gbc_progressBar.insets = new Insets(0, 0, 5, 0);
-			gbc_progressBar.gridx = 0;
-			gbc_progressBar.gridy = 0;
-			contentPanel.add(progressBar, gbc_progressBar);
-		}
-		{
-			lblStatus = new JLabel("Status");
-			GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-			gbc_lblNewLabel.gridx = 0;
-			gbc_lblNewLabel.gridy = 2;
-			contentPanel.add(lblStatus, gbc_lblNewLabel);
-		}
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
-	}
+	    super(parentFrame, "Progress Dialog", true);
+	    dpb = new JProgressBar(0, maxCount);
+	    lblProgress = new JLabel("Progress...");
+	    lblStatus = new JLabel("Status...");
+	    this.add(BorderLayout.CENTER, dpb);
+	    this.add(BorderLayout.NORTH, lblProgress);
+	    this.add(BorderLayout.SOUTH, lblStatus);
+	    this.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+	    this.setSize(300, 120);
+	    this.setLocationRelativeTo(parentFrame);
 
-	public void startP() {
-//		progressBar.setMaximum(files.size());
-		int c = 1;
-		for (File file : files) {
-			// System.out.println("file " + c++ + "("+ file.getName() +")"+ "
-			// sent for processing...");
-//			progressBar.setValue(c);
-			RenameService.start(
-					file.getAbsolutePath().substring(0, file.getAbsolutePath().length() - file.getName().length()),
-					file.getName(), prefix, postfix, includeDate, includeTime, includeLocation);
-		System.out.println(c);
-		c++;
-		}
-		this.dispose();
-	}
-	// System.out.println("Rename process of " + files.size() + " files
-	// Completed!");
-
+	}	
 }
